ad0f2e22ba81daf97bbbfd2238c9b0bf0e017048
Andrea De La Cruz <acruz@digitalgeko.com>
2016-01-28 12:21:03 -0600

    MongoDB replica seeds connection

--- src/main/groovy/nl/viking/db/MorphiaFactory.groovy
+++ src/main/groovy/nl/viking/db/MorphiaFactory.groovy
@@ -1,5 +1,6 @@
 package nl.viking.db
 
+import com.gmongo.GMongoClient
 import com.mongodb.Mongo
 import com.mongodb.MongoClient
 import com.mongodb.MongoCredential
@@ -29,7 +30,18 @@ class MorphiaFactory {
 				def credentials = MongoCredential.createCredential(GMongoProps.getDBUsername(), GMongoProps.getDBName(), GMongoProps.getDBPassword().toCharArray())
 				credentialsList.add(credentials)
 			}
-			def mongo = new MongoClient(new ServerAddress(GMongoProps.getDBHost(), GMongoProps.getDBPort()), credentialsList)
+
+			def mongo
+			def serverAddresses = GMongoProps.getDBServerAddresses()
+			if (serverAddresses) {
+				def seeds = serverAddresses.collect {
+					new ServerAddress(it.host, it.port ?: GMongoProps.getDBPort())
+				}
+				mongo = new GMongoClient(seeds, credentialsList)
+			} else {
+				mongo = new MongoClient(new ServerAddress(GMongoProps.getDBHost(), GMongoProps.getDBPort()), credentialsList)
+			}
+
 			dataStore = new Morphia().createDatastore(mongo, GMongoProps.getDBName())
 		}
 		return dataStore
